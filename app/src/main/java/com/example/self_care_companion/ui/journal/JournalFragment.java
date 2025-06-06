bd891461ddc852bba3532b0e568735366c2db184
Simone Coutinho <sscoutin@uwaterloo.ca>
2025-06-06 14:08:17 -0400

    Add temp nav bar feature

--- app/src/main/java/com/example/self_care_companion/ui/journal/JournalFragment.java
+++ app/src/main/java/com/example/self_care_companion/ui/journal/JournalFragment.java
@@ -1,4 +1,4 @@
-package com.example.self_care_companion.ui.dashboard;
+package com.example.self_care_companion.ui.journal;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.self_care_companion.databinding.FragmentDashboardBinding;
+import com.example.self_care_companion.databinding.FragmentJournalBinding;
 
-public class DashboardFragment extends Fragment {
+public class JournalFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentJournalBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        JournalViewModel JournalViewModel =
+                new ViewModelProvider(this).get(JournalViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentJournalBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textJournal;
+        JournalViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
